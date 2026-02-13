264c06709df0da48147aa1f23fcdd9514b828495
swetha28 <swechess97@gmail.com>
2026-02-13 11:35:48 +0000

    add ecs cluster

--- terraform_modules/ecs_cluster/output.tf
+++ terraform_modules/ecs_cluster/output.tf
@@ -1,3 +1,14 @@
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.this.name
+output "cluster_id" {
+  description = "The ECS cluster ID"
+  value       = aws_ecs_cluster.this.id
+}
+
+output "cluster_arn" {
+  description = "The ECS cluster ARN"
+  value       = aws_ecs_cluster.this.arn
+}
+
+output "cluster_name" {
+  description = "The ECS cluster name"
+  value       = aws_ecs_cluster.this.name
 }
--- variable.tf
+++ variable.tf
@@ -1,7 +1,14 @@
-variable "environment" {
-  
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
 }
 
 variable "resource_prefix" {
-  
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev/staging/prod)"
+  type        = string
 }
