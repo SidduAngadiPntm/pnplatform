0c2b165102914da0465cb2ab8b064fad146180e4
Siddu Angadi <Shiddalingeshangadi.angadi@com>
2026-02-13 18:37:25 +0000

    added vpc

--- business_services/gitlab_runners/output.tf
+++ business_services/gitlab_runners/output.tf
@@ -2,17 +2,7 @@
 # VPC Module Outputs
 ################################################################################
 
-output "vpc_id" {
+output "vpc_details" {
   description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnet_1_id" {
-  description = "Public subnet 1 ID"
-  value       = module.vpc.public_subnet_1_id
-}
-
-output "internet_gateway_id" {
-  description = "Internet Gateway ID"
-  value       = module.vpc.internet_gateway_id
+  value       = module.vpc
 }
